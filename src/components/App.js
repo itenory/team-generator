33c4cbdd21e117f068a87e17df12d973d2b18612
Tyrone Williams <itenory@gmail.com>
2018-11-26 23:11:57 -0500

    Adds to and display player list

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import PlayerList from './PlayerList';
+import SearchBar from './SearchBar';
 
 class App extends Component {
-  state = {};
+  state = { players: {} };
+
+  addPlayers = names => {
+    const playersToAdd = names.split(','); // separate multiple players with ','
+    const playerList = { ...this.state.players };
+
+    // Adds new users to list it not a duplicate.
+    playersToAdd.map(name => {
+      if (!playerList[name.trim()]) {
+        playerList[name.trim()] = { name: name.trim() };
+      }
+      return name;
+    });
+
+    this.setState({ players: playerList });
+  };
 
   render() {
     return (
       <main>
-        <div>app</div>
+        <PlayerList players={this.state.players} />
+        <SearchBar onSubmit={this.addPlayers} />
       </main>
     );
   }
