5dd1c060870445ce0703628eaf3d9b96a09a9c9d
Tyrone Williams <itenory@gmail.com>
2018-11-26 23:13:01 -0500

    Displays player list

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PlayerList = props => <div>player list</div>;
+const PlayerList = props => {
+  // No IE Support, must change later.
+  const listItems = Object.values(props.players).map(player => (
+    <li key={`player-${player.name}`}>{player.name}</li>
+  ));
 
+  return <ul>{listItems}</ul>;
+};
+
+PlayerList.propTypes = {
+  players: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
 export default PlayerList;
